75ede35ad3982e534ec15cfdcbd36433b44cbb94
sravan github <sravankumar04032001@gmail.com>
2024-01-24 11:17:08 +0530

    This is the file for python practice

--- Python/23-01-2024.py
+++ Python/23-01-2024.py
@@ -1,18 +1,14 @@
 #Find the sum of the second largest number in a sub-list => Input = [[1, 2, 3], [2, 4, 1, 0]] Output = 2+2 = 4
 
-def sum(input):
-    result = 0
+nested_list = [[1, 2, 3], [2, 4, 1, 0]]
 
-    for sublist in input:
-        sublist.sort()
-        if len(sublist) > 1:
-            result += sublist[-2]
+# Printing all the elements of a nested list
+for sublist in nested_list:
+    sublist.sort()
+a=nested_list[0][-2] + nested_list[1][-2]
+print(a)
 
-    return result
 
-my_list = [[1, 2, 3], [2, 4, 1, 0]]
-sum = sum(my_list)
-print(sum)
 
 
 
